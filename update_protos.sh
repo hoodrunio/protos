ec2a0de78859ee882ef4bdd3edd4450e8328da48
Errorist <71728280+Errorist79@users.noreply.github.com>
2024-02-14 02:00:16 +0300

    Update update_protos.sh

--- update_protos.sh
+++ update_protos.sh
@@ -1,35 +1,10 @@
-buf export buf.build/evmos/evmos --output .
-buf export buf.build/osmosis-labs/osmosis --output .
-# buf export buf.build/kyve//chain --output .
 buf export buf.build/cosmwasm/wasmd --output .
 
 mkdir temp
 cd temp
 
-git clone https://github.com/umee-network/umee
-buf export umee/proto --output ../
-
-git clone https://github.com/Gravity-Bridge/Gravity-Bridge
-buf export Gravity-Bridge/module/proto --output ../
-
-git clone https://github.com/ingenuity-build/quicksilver
-buf export quicksilver/proto --output ../
-
-git clone https://github.com/KYVENetwork/chain
-buf export chain/proto --output ../
-
-git clone https://github.com/lavanet/lava
-mkdir -p ../lavanet/lava
-cp -r lava/proto/* ../lavanet/lava
-
 git clone https://github.com/axelarnetwork/axelar-core
 cp -r axelar-core/proto/* ../
-
-git clone https://github.com/chain4energy/c4e-chain
-cp -r c4e-chain/proto/* ../
-
-git clone https://github.com/Agoric/agoric-sdk
-cp -r agoric-sdk/golang/cosmos/proto/* ../
 
 git clone https://github.com/babylonchain/babylon
 cp -r babylon/proto/* ../
